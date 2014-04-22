e36b85f6c3763fe3c9b68c974a789f3a66a67a2e
Robin Stenvi <robins54@hotmail.com>
2014-04-22 17:08:08 +0200

    Fixed some issues when compiling on 64-bit system

--- create_image.sh
+++ create_image.sh
@@ -14,14 +14,14 @@ mkdir floppy/boot/grub
 cp grub/stage1 floppy/boot/grub
 cp grub/stage2 floppy/boot/grub
 cp grub/menu.lst floppy/boot/grub
-sudo umount floppy
 
-
-grub --batch <<EOT
+grub --batch --device-map=/dev/null <<EOT
 device (fd0) floppy.img
-install (fd0)/boot/grub/stage1 (fd0) (fd0)/boot/grub/stage2
-(fd0)/boot/grub/menu.lst
+root (fd0)
+setup (fd0)
 quit
 EOT
+
+sudo umount floppy
 
 rmdir floppy
